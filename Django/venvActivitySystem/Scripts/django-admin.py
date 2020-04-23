a94e72d17fadfd5fcf41dd539962a6ca818a9342
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-04-23 16:38:08 +1200

    Django Install/Config
    Django Install/Config

---
+++ Django/venvActivitySystem/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\iscg 7427\assignment\activitymanagementsystem\django\venvactivitysystem\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
