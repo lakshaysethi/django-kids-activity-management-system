5513e8cf367e04ff4ab6bce292917683bfa243a7
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-23 16:58:03 +1200

    commit

--- django Activity management system/django/ass1/ams/views.py
+++ django Activity management system/django/ass1/ams/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+
+
 def home(request):
     return render(request,'home.html')
+
+
+
+def child_profile(request):
+    return render(request,'child-profile.html')
+
+
--- django Activity management system/django/ass1/ass1/urls.py
+++ django Activity management system/django/ass1/ass1/urls.py
@@ -1,10 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path
+
 from ams import views
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-
+    path('', views.home, name='home'),
+    path('child/profile/', views.child_profile, name='child-profile'),
 
 ]
