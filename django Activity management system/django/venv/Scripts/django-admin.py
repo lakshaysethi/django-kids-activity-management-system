fb296f3247b67349b1e29398184edfda9449ccd9
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-23 16:32:54 +1200

    dfgs

---
+++ django Activity management system/django/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\laksh\desktop\django-kids-activity-management-system\django activity management system\django\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
