7b2560b8a499583dd0cbb0b213902ce3a187fd65
Lakshay Sethi <lakshay.new@gmail.com>
2020-05-18 20:12:04 +1200

    fixed static

--- ass1/ass1/settings.py
+++ ass1/ass1/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STARTC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 AUTH_USER_MODEL = 'ams.User'
 
