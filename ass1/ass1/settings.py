52e62b276a006ed89a00eb28590c47d1dd796f32
Lakshay Sethi <lakshay.new@gmail.com>
2020-05-18 20:17:50 +1200

    fixed static

--- ass1/ass1/settings.py
+++ ass1/ass1/settings.py
@@ -119,9 +119,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
 STARTC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATIC_URL = '/static/'
+
 
 AUTH_USER_MODEL = 'ams.User'
 
